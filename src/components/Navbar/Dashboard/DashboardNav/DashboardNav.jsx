28768d104b2264e5635a7ced5a288ecbd95f499f
abuabdullah23 <abuobaidazayed@gmail.com>
2023-08-25 15:05:15 +0600

    ActiveLink setup for dashboard

--- src/components/Navbar/Dashboard/DashboardNav/DashboardNav.jsx
+++ src/components/Navbar/Dashboard/DashboardNav/DashboardNav.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import NavLink from '../../ActiveLink/NavLink';
 
 const DashboardNav = () => {
     const navLinks = [
@@ -23,17 +24,19 @@ const DashboardNav = () => {
     ]
 
     return (
-        <div>
+        <div className='w-1/6'>
             <h1 className='text-3xl font-semibold'>Dashboard</h1>
             <div>
                 {
                     navLinks.map(({ path, title }) => (
                         <li key={path}
-                        className='my-3 list-none text-gray-500'
+                            className='my-3 list-none text-gray-500'
                         >
-                            <Link href={path}
-                                className='hover:text-black hover:border-b-2 border-black transition duration-300'
-                            >{title}</Link>
+                            <NavLink href={path}
+                                exact
+                                activeClassName='bg-blue-600 text-white py-2 px-4 w-full'
+                                className='hover:text-gray-200 hover:bg-blue-600 transition duration-300 py-2 px-4'
+                            >{title}</NavLink>
                         </li>
                     ))
                 }
