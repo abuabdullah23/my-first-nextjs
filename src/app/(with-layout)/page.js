1929de28366772c0f9fa861e5042b1ab922dbeb9
abuabdullah23 <abuobaidazayed@gmail.com>
2023-08-26 10:33:27 +0600

    Next Image Components, blur image

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,7 +1,29 @@
+import image1 from "@/assets/marguerite-729510_640.jpg";
+import Image from "next/image";
+
 const HomePage = () => {
   return (
     <main>
       <h2>Home</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-7">
+        {/* image from assets */}
+        <Image
+        src={image1} 
+        placeholder="blur" 
+        alt="Image 1" 
+        />        
+        
+
+        {/* image from url */}
+        <Image 
+        className="h-full object-cover" 
+        width={400} 
+        height={300}        
+        src="https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHJlZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" 
+        alt="" />
+      
+        
+      </div>
     </main>
   )
 }
